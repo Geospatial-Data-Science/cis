344086d8cf192f640ff781e0752e08fe09027aba
mmann1123 <mmann1123@gmail.com>
2020-02-12 14:15:04 -0500

    adding MODIS_L3_Land class

--- cis/data_io/products/MODIS.py
+++ cis/data_io/products/MODIS.py
@@ -59,6 +59,142 @@ def _apply_scaling_factor_MODIS(data, scale_factor, offset):
     return (data - offset) * scale_factor
 
 
+
+
+class MODIS_L3_Land(AProduct):
+    """
+    Data product for MODIS Level 3 data vegetation products
+    """
+
+    def _parse_datetime(self, metadata_dict, keyword):
+        import re
+        res = ""
+        for s in metadata_dict.values():
+            i_start = s.find(keyword)
+            ssub = s[i_start:len(s)]
+            i_end = ssub.find("END_OBJECT")
+            ssubsub = s[i_start:i_start + i_end]
+            matches = re.findall('".*"', ssubsub)
+            if len(matches) > 0:
+                res = matches[0].replace('\"', '')
+                if res is not "":
+                    break
+        return res
+
+    def _get_start_date(self, filename):
+        from cis.parse_datetime import parse_datetimestr_to_std_time
+        metadata_dict = hdf.get_hdf4_file_metadata(filename)
+        date = self._parse_datetime(metadata_dict, 'RANGEBEGINNINGDATE')
+        time = self._parse_datetime(metadata_dict, 'RANGEBEGINNINGTIME')
+        datetime_str = date + " " + time
+        return parse_datetimestr_to_std_time(datetime_str)
+
+    def _get_end_date(self, filename):
+        from cis.parse_datetime import parse_datetimestr_to_std_time
+        metadata_dict = hdf.get_hdf4_file_metadata(filename)
+        date = self._parse_datetime(metadata_dict, 'RANGEENDINGDATE')
+        time = self._parse_datetime(metadata_dict, 'RANGEENDINGTIME')
+        datetime_str = date + " " + time
+        return parse_datetimestr_to_std_time(datetime_str)
+
+    def get_file_signature(self):
+        product_names = [ "MCD15A2"]
+        regex_list = [r'.*' + product + '.*\.hdf' for product in product_names]
+        return regex_list
+
+    def get_variable_names(self, filenames, data_type=None):
+        try:
+            from pyhdf.SD import SD
+        except ImportError:
+            raise ImportError("HDF support was not installed, please reinstall with pyhdf to read HDF files.")
+
+        variables = set([])
+        for filename in filenames:
+            sd = SD(filename)
+            for var_name, var_info in sd.datasets().items():
+                # Check that the dimensions are correct
+                if var_info[0] == ('YDim_MOD_Grid_MOD15A2H', 'XDim_MOD_Grid_MOD15A2H'):
+                    variables.add(var_name)
+
+        return variables
+
+    def _create_cube(self, filenames, variable):
+        import numpy as np
+        from cis.data_io.hdf import _read_hdf4
+        from iris.cube import Cube, CubeList
+        from iris.coords import DimCoord, AuxCoord
+        from cis.time_util import calculate_mid_time, cis_standard_time_unit
+        from cis.data_io.hdf_sd import get_metadata
+        from cf_units import Unit
+
+        variables = ['XDim', 'YDim', variable]
+        logging.info("Listing coordinates: " + str(variables))
+
+        cube_list = CubeList()
+        # Read each file individually, let Iris do the merging at the end.
+        for f in filenames:
+            sdata, vdata = _read_hdf4(f, variables)
+
+            lat_coord = DimCoord(_get_MODIS_SDS_data(sdata['YDim_MOD_Grid_MOD15A2H']), standard_name='latitude', units='degrees')
+            lon_coord = DimCoord(_get_MODIS_SDS_data(sdata['XDim_MOD_Grid_MOD15A2H']), standard_name='longitude', units='degrees')
+
+            # create time coordinate using the midpoint of the time delta between the start date and the end date
+            start_datetime = self._get_start_date(f)
+            end_datetime = self._get_end_date(f)
+            mid_datetime = calculate_mid_time(start_datetime, end_datetime)
+            logging.debug("Using {} as datetime for file {}".format(mid_datetime, f))
+            time_coord = AuxCoord(mid_datetime, standard_name='time', units=cis_standard_time_unit,
+                                  bounds=[start_datetime, end_datetime])
+
+            var = sdata[variable]
+            metadata = get_metadata(var)
+
+            try:
+                units = Unit(metadata.units)
+            except ValueError:
+                logging.warning("Unable to parse units '{}' in {} for {}.".format(metadata.units, f, variable))
+                units = None
+
+            cube = Cube(_get_MODIS_SDS_data(sdata[variable]),
+                        dim_coords_and_dims=[(lon_coord, 1), (lat_coord, 0)],
+                        aux_coords_and_dims=[(time_coord, None)],
+                        var_name=metadata._name, long_name=metadata.long_name, units=units)
+
+            cube_list.append(cube)
+
+        # Merge the cube list across the scalar time coordinates before returning a single cube.
+        return cube_list.merge_cube()
+
+    def create_coords(self, filenames, variable=None):
+        """Reads the coordinates on which a variable depends.
+        Note: This calls create_data_object because the coordinates are returned as a Cube.
+        :param filenames: list of names of files from which to read coordinates
+        :param variable: name of variable for which the coordinates are required
+        :return: iris.cube.Cube
+        """
+        if variable is None:
+            variable_names = self.get_variable_names(filenames)
+            variable_name = str(variable_names.pop())
+            logging.debug("Reading an IRIS Cube for the coordinates based on the variable %s" % variable_names)
+        else:
+            variable_name = variable
+
+        return self.create_data_object(filenames, variable_name)
+
+    def create_data_object(self, filenames, variable):
+        from cis.data_io.gridded_data import make_from_cube
+        logging.debug("Creating data object for variable " + variable)
+
+        cube = self._create_cube(filenames, variable)
+        return make_from_cube(cube)
+
+    def get_file_format(self, filename):
+        return "HDF4/ModisL3"
+
+
+
+
+
 class MODIS_L3(AProduct):
     """
     Data product for MODIS Level 3 data
@@ -96,7 +232,7 @@ class MODIS_L3(AProduct):
         return parse_datetimestr_to_std_time(datetime_str)
 
     def get_file_signature(self):
-        product_names = ['MYD08_D3', 'MOD08_D3', 'MYD08_M3', 'MOD08_M3', "MOD08_E3", "MCD15A2H"]
+        product_names = ['MYD08_D3', 'MOD08_D3', 'MYD08_M3', 'MOD08_M3', "MOD08_E3", "MCD15A2"]
         regex_list = [r'.*' + product + '.*\.hdf' for product in product_names]
         return regex_list
 
